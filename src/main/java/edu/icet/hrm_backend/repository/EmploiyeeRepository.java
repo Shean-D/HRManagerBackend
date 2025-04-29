1f26636f51f760c9f2200eca52fa50a539bdec95
Shean-D <sheandivyan01@gmail.com>
2025-04-29 11:21:37 +0530

    EmployeeRepository interface created and extends JPARepository

---
+++ src/main/java/edu/icet/hrm_backend/repository/EmploiyeeRepository.java
@@ -0,0 +1,8 @@
+package edu.icet.hrm_backend.repository;
+
+import edu.icet.hrm_backend.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmploiyeeRepository extends JpaRepository<EmployeeEntity, Long> {
+
+}
